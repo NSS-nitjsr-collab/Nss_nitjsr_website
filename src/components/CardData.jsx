72c9529915e73089c1653a3376baf91f17322716
hiteshkumar8642 <hiteshkumar8642@gmail.com>
2023-08-18 18:40:20 +0530

    teams page added

---
+++ src/components/CardData.jsx
@@ -0,0 +1,157 @@
+//  import React from 'react'
+ 
+
+
+
+ const CardData=[
+    {
+        img:"https://drive.google.com/uc?id=1JNJyt01AkDup61zRCrBlrbemJ92Ia7GA",
+        title:"Ronit Ranjan",
+        position:"President",
+        discription:"An engineer who uses his technical skills to make a positive impact on society through his dedicated work in social service initiatives."
+    },
+    {
+        img:"https://drive.google.com/uc?id=1UBhLAgFqK6dFhpwvPOIGxf9UzDSwh8Ul",
+        title:"ADITI PRASAD",
+        position:"Vice President",
+        discription:"Nature Nurturer!"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1vmJMb0F8cYt_H3LkgBI6nCeHlfy615sP",
+        title:"Arya Aryan",
+        position:"EM Head",
+        discription:"Leading from front"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1GN2JmItCxXKsWNE5EwhkdAVrtVFZ1dak",
+        title:"Aman Kumar ",
+        position:"EM Head",
+        discription:"I am ecstatic and proud member of NSS NIT JAMSHEDPUR."
+    },
+    {
+        img:"https://drive.google.com/uc?id=1U0puh1BjzGm7Zzy5ammt_46HKCpzKPLe",
+        title:"Pawar Ajay ",
+        position:"Strategic and Planning Head",
+        discription:"Diligently work with compassion"
+    },
+    {
+        img:"https://drive.google.com/uc?id=16_iakExmeSgf8T2E5yfEqtaDNFcOvOy0",
+        title:"Shubham Kumar ",
+        position:"Strategic and Planning Head",
+        discription:"Diligently work with compassion"
+    },
+    {
+        img:"https://drive.google.com/uc?id=16Gjn7626_TzzQ-GrZ68n2Au1wuTGnCye",
+        title:"YENNI SRUJANA ",
+        position: "Creative Head",
+        discription:"I am a dedicated and an optimistic person."
+    },
+    {
+        img:"https://drive.google.com/uc?id=1H1pAlGYqNPUxYJvh3GuqTpGbjPOmeQcS",
+        title:"Bishal Kumar Shil",
+        position: "PR Head",
+        discription:"Learning from the new realms of the modern world"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1qBQMylu0sOQxI-7zhFiN21WQq-YJJph2",
+        title:"YASHRAJ SINGH",
+        position: "PR Head",
+        discription:"Passionate, dedicated, simple and always down to earth and always believe in simple living and high thinking. "
+    },
+    {
+        img:"https://drive.google.com/uc?id=1-bBdI_Qcl00qZXzVzd75Q05SNE5vRs5L",
+        title:"Ravi Mandal",
+        position: "Media Head",
+        discription:"Helping hands are better than the praying lips"
+    },
+    {
+        img:"https://drive.google.com/uc?id=17xwRdyLXlRbr4mq1P3Va_VzDRzFEqF2J",
+        title:"Sulav Kumar ",
+        position: "Media Head",
+        discription:"Helping hands are better than the praying lips"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1A2ggXRtUPrP0hjsOKyXiTnA-0yAdEmBf",
+        title:"Guddu Kumar",
+        position: "Web Head",
+        discription:"Optimistic person"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1VaQtMum5KOwmvXYfbrsm8uKWKH24I6cM",
+        title:"Hitesh Ahuja",
+        position: "Pg representative",
+        discription:"Learning from the new realms of the modern world"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1rpI4sbFwwGMoiC31pT9xl5rU_-1BsZkD",
+        title:"Chiranjiv Kumar",
+        position: "Pg representative",
+        discription:"Learning from the new realms of the modern world"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1zTQjwkTHJI457dZy1KjOzlQFhWVnn5hp",
+        title:"INDRANIL CHATTERJEE  ",
+        position: "General Secretary",
+        discription:"I am kind, polite and passionate about my work."
+    },
+    {
+        img:"https://drive.google.com/uc?id=1GLuxl22vIH9KJMTF-P7SqZnyTtLP-xS9",
+        title:"Satyam Shukla",
+        position: "General Secretary",
+        discription:"Amazing bonding among teammates"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1WsG3H0c5E8M4DvVYp8wXnXLGk2O4UD9n",
+        title:"Umesh Nayak ",
+        position: "JOINT SECRETARY",
+        discription:"I'm  a unique individual with a vibrant personality and a thirst for knowledge. My curiosity drives me  to explore new ideas and engage in meaningful conversations."
+    },
+    {
+        img:"https://drive.google.com/uc?id=1HXsccWokKCWDprgg72szEOS2gZX5tLaX",
+        title:"Shubham Mishra",
+        position: "JOINT SECRETARY",
+        discription:"Hard working and Dedicated person"
+    },
+    
+    {
+        img:"https://drive.google.com/uc?id=1VemtVrYY5LD6QF7Hj9Apu1sd-qzE8Rgl",
+        title:"Anurag Sarkar",
+        position: "JOINT SECRETARY",
+        discription:"Yare yare daze"
+    },
+    
+    {
+        img:"https://drive.google.com/uc?id=1MKUXhM90pkqC1_QArEnCTzeK0nFQYXzJ",
+        title:"Rinku Kumari ",
+        position: "JOINT SECRETARY",
+        discription:""
+    },
+    {
+        img:"https://drive.google.com/uc?id=1LdOP_ItBxlb3TnCUq0zo4tMRoaEwiHDQ",
+        title:"Ankit xess",
+        position: "JOINT SECRETARY",
+        discription:"Highly determined volunteer "
+    },
+    {
+        img:"https://drive.google.com/uc?id=1eYsZkGq9b6kWuLETF3Th7DEpdHD_c11F",
+        title:"DEEPANSHU GARG ",
+        position: "JOINT SECRETARY",
+        discription:"I do not give up until I achieve it ! "
+    },
+    {
+        img:"https://drive.google.com/uc?id=1_ANsoMigM7n4yTixyAXLSpRvrMsP_AZA",
+        title:"Sweety Rani",
+        position: "JOINT SECRETARY",
+        discription:"A disciplined student, dedicated to excellence, shaping a path of success with determination and focus"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1iBUZ5OZ9AT2VplmrS9MB-jYgHEI0dkJD",
+        title:"KHUSHEE ",
+        position: "JOINT SECRETARY",
+        discription:"Optimistic person"
+    }
+ ]
+
+
+ export default CardData;
+ 
---
+++ src/components/CardData2.jsx
@@ -0,0 +1,92 @@
+//  import React from 'react'
+ 
+
+
+
+const CardData2=[
+    {
+        img:"https://drive.google.com/uc?id=1JNJyt01AkDup61zRCrBlrbemJ92Ia7GA",
+        title:"Ronit Ranjan",
+        position:"President",
+        discription:"An engineer who uses his technical skills to make a positive impact on society through his dedicated work in social service initiatives."
+    },
+    {
+        img:"https://drive.google.com/uc?id=1UBhLAgFqK6dFhpwvPOIGxf9UzDSwh8Ul",
+        title:"ADITI PRASAD",
+        position: "VICE PRESIDENT",
+        discription:"Nature Nurturer!"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1A2ggXRtUPrP0hjsOKyXiTnA-0yAdEmBf",
+        title:"Guddu Kumar",
+        position: "Web Head",
+        discription:"Optimistic person"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1VaQtMum5KOwmvXYfbrsm8uKWKH24I6cM",
+        title:"Hitesh Ahuja",
+        position: "Pg representative",
+        discription:"Learning from the new realms of the modern world"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1eYsZkGq9b6kWuLETF3Th7DEpdHD_c11F",
+        title:"DEEPANSHU GARG ",
+        position: "JOINT SECRETARY",
+        discription:"I do not give up until I achieve it ! "
+    },
+    {
+        img:"https://drive.google.com/uc?id=1AoyTQhWJBscb-6fU4EEcBdJt7SgnVaTT",
+        title:"Hitesh Kumar",
+        position: "Member",
+        discription:"A SELF MOTIVATED CODER WITH AN EAGERNESS TO LEARN AND GROW"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1znkJhrD-JTfdGzIWL5uKxX7DvdhtyOP4",
+        title:"Somesh Ghosh",
+        position: "Member",
+        discription:"Loves Tech | Full Stack Developer| DSA, CP and ML Enthusiast"
+    },
+    {
+        img:"https://drive.google.com/uc?id=1c3IhhL1zPi8U69rZPvtS-AEOfwBTT-Yn",
+        title:"Gattamneni Abhiram ",
+        position: "Member",
+        discription:""
+    },
+    {
+        img:"https://drive.google.com/uc?id=1SabMTbBR7VUfXDgnSHPl59queszl3MiS",
+        title:"Aakash kumar",
+        position: "Member",
+        discription:""
+    },
+    {
+        img:"https://drive.google.com/uc?id=17wGkc7FXmMQR-k1RZ-86zjsLurECcZUR",
+        title:"Ritik Bansal",
+        position: "Member",
+        discription:""
+    },
+    {
+        img:"https://drive.google.com/uc?id=1seuHXMgMZ7I1o433B0qJhWmxkbFcXj3K",
+        title:"Deep sen",
+        position: "Member",
+        discription:""
+    },
+    
+    {
+        img:"https://drive.google.com/uc?id=1Xko2CXR5fFzcH5oNJd6Uwyrps8XQIhx7",
+        title:"Ankit Singh",
+        position: "Member",
+        discription:"I am hard working"
+    },
+    {
+        img:"https://drive.google.com/uc?id=14gJGPEbETsSCysUWj8oPkOGmHRGJfoa1",
+        title:"Aditya agrawal",
+        position: "Member",
+        discription:"I am hard working"
+    },
+    
+    
+ ]
+
+
+ export default CardData2;
+ 
