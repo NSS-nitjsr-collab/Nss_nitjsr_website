e798b92a1786edeaacdfdecb95bc78fe28b7c87e
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2023-10-02 17:16:14 +0530

    styled footer and removed unnecessary files/code

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,19 +7,18 @@ import {
   Typography,
   Button,
   IconButton,
-  Card,
 } from "@material-tailwind/react";
- 
+
 export default function Header() {
   const [openNav, setOpenNav] = React.useState(false);
- 
+
   React.useEffect(() => {
     window.addEventListener(
       "resize",
       () => window.innerWidth >= 960 && setOpenNav(false)
     );
   }, []);
- 
+
   const navList = (
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
@@ -29,9 +28,8 @@ export default function Header() {
         className="p-1 font-normal"
       >
         <NavLink to="/" className="nav-link ">
-                  Home
+          Home
         </NavLink>
-       
       </Typography>
       <Typography
         as="li"
@@ -40,7 +38,7 @@ export default function Header() {
         className="p-1 font-normal"
       >
         <NavLink to="/about" className="nav-link ">
-                  About
+          About
         </NavLink>
       </Typography>
       <Typography
@@ -50,7 +48,7 @@ export default function Header() {
         className="p-1 font-normal"
       >
         <NavLink to="/event" className="nav-link ">
-                  Event
+          Event
         </NavLink>
       </Typography>
       <Typography
@@ -60,7 +58,7 @@ export default function Header() {
         className="p-1 font-normal"
       >
         <NavLink to="/team" className="nav-link ">
-                  Team
+          Team
         </NavLink>
       </Typography>
       <Typography
@@ -70,12 +68,12 @@ export default function Header() {
         className="p-1 font-normal"
       >
         <NavLink to="/contact" className="nav-link ">
-                  Contact
+          Contact
         </NavLink>
       </Typography>
     </ul>
   );
- 
+
   return (
     <>
       <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
@@ -87,9 +85,8 @@ export default function Header() {
             className="mr-4 cursor-pointer font-bold py-1.5 "
           >
             <Link to="/" className="navbar-brand">
-               NSS NIT Jamshedpur
+              NSS NIT Jamshedpur
             </Link>
-            
           </Typography>
           <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">{navList}</div>
